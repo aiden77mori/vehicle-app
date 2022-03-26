1623795d9aad2c8db943026da65a06e35101c6fd
hellovlady <danillppiont106@gmail.com>
2022-03-27 03:16:14 +0900

    frotnend modification

--- layout/scripts/global.js
+++ layout/scripts/global.js
@@ -1,16 +1,36 @@
 $(function () {
+  /** when click profile button */
   $(".user_box").on("click", function () {
     var html = "";
     html += '<div class="user_modal_container">';
     html += '<div class="modal_content">';
     html += '<div class="modal_header">';
-    html += "<p>modyfikowanie informacji o użytkowniku</p>";
+    html += "<p>EDYCJA PROFILU</p>";
     html += "</div>";
     html += '<div class="modal_card">';
+
+    html += '<div class="user_info_field">';
+    html += "<p>Nazwa Użytkownika / login</p>";
+    html += '<input type="text" class="search_form username" value="vlady" />';
+    html += "</div>";
+
+    html += '<div class="user_info_field">';
+    html += "<p>Imię i nazwisko</p>";
     html +=
-      '<input type="text" placeholder="" class="search_form username" value="Nazwa" />';
+      '<input type="text" class="search_form user_fullname" value="Vladyslav Parkhomenko" />';
+    html += "</div>";
+
+    html += '<div class="user_info_field">';
+    html += "<p>Email</p>";
     html +=
-      '<input type="text" placeholder="" class="search_form password" value="hasło" />';
+      '<input type="text" class="search_form email" value="vlady@gmail.com" />';
+    html += "</div>";
+    html += '<div class="user_info_field">';
+    html += "<p>Nowe hasło</p>";
+    html +=
+      '<input type="password" class="search_form password" value="hasło" />';
+    html += "</div>";
+
     html += '<div class="modal_footer">';
     html += '<button class="cancel modify_btn">Anuluj</button>';
     html += '<button class="agree modify_btn">modyfikować</button>';
@@ -25,6 +45,39 @@ $(function () {
     $(".modify_btn").on("click", function () {
       $(".user_modal_container").hide();
       $(".user_modal_container").remove();
+    });
+  });
+
+  /**  when click log out button */
+  $(".logout_box").on("click", function () {
+    var html = "";
+    html += '<div class="logout_modal_container">';
+    html += '<div class="modal_content">';
+    html += '<div class="modal_header">';
+    html += '<img src="../../images/sysadmin/warn.svg" alt="warn" />';
+    html += "</div>";
+    html += '<div class="modal_card">';
+    html += '<div class="warn_info">';
+    html += "<h1>CZY NA PEWNO CHCESZ SIĘ WYLOGOWAĆ?</h1>";
+    html += "</div>";
+    html += '<div class="modal_footer">';
+    html += '<button class="cancel modify_btn">Anuluj</button>';
+    html += '<button class="agree modify_btn">WYLOGUJ</button>';
+    html += "</div>";
+    html += "</div>";
+    html += "</div>";
+    html += "</div>";
+
+    $("body").append(html);
+    $(".logout_modal_container").show();
+    $(".logout_modal_container .cancel").on("click", function () {
+      $(".logout_modal_container").hide();
+      $(".logout_modal_container").remove();
+    });
+    $(".logout_modal_container .agree").on("click", function () {
+      $(".logout_modal_container").hide();
+      $(".logout_modal_container").remove();
+      // window.location.href = "../../index.html";
     });
   });
 });
